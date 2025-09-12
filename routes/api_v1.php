c21702f32abc302d136db8de855fe748573d08ef
syamimhazmi <syamimhazmi@gmail.com>
2025-09-12 23:53:17 +0700

    cs fixer

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,14 +1,11 @@
 <?php
 
 use App\Http\Controllers\KeyValueStoreController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/object', [KeyValueStoreController::class, 'store'])
     ->middleware('throttle:100,1');
 
 Route::get('/object/get_all_records', [KeyValueStoreController::class, 'getAllRecords']);
+
 Route::get('/object/{key}', [KeyValueStoreController::class, 'show']);
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
